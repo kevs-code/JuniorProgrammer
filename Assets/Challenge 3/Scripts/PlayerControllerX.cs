c053fe4a9e152db33433c5a57a5d546362ae9bc9
kevs-code <kevscode@hotmail.com>
2025-03-28 11:45:49 +0000

    Challenge3

--- Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -1,65 +1,78 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerControllerX : MonoBehaviour
+namespace ChallengeThree
 {
-    public bool gameOver;
-
-    public float floatForce;
-    private float gravityModifier = 1.5f;
-    private Rigidbody playerRb;
-
-    public ParticleSystem explosionParticle;
-    public ParticleSystem fireworksParticle;
-
-    private AudioSource playerAudio;
-    public AudioClip moneySound;
-    public AudioClip explodeSound;
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerControllerX : MonoBehaviour
     {
-        Physics.gravity *= gravityModifier;
-        playerAudio = GetComponent<AudioSource>();
+        private float gravityModifier = 1f;
+        private Rigidbody playerRb;
+        private AudioSource playerAudio;
 
-        // Apply a small upward force at the start of the game
-        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+        public bool gameOver = true;
+        public float floatForce = 2.5f;
+        public float yLowerRange = 2f;
+        public float yUpperRange = 14f;
+        public ParticleSystem explosionParticle;
+        public ParticleSystem fireworksParticle;
 
-    }
+        public AudioClip moneySound;
+        public AudioClip explodeSound;
+        public AudioClip bounceSound;
 
-    // Update is called once per frame
-    void Update()
-    {
-        // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        // Start is called before the first frame update
+        void Start()
         {
-            playerRb.AddForce(Vector3.up * floatForce);
-        }
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        // if player collides with bomb, explode and set gameOver to true
-        if (other.gameObject.CompareTag("Bomb"))
-        {
-            explosionParticle.Play();
-            playerAudio.PlayOneShot(explodeSound, 1.0f);
-            gameOver = true;
-            Debug.Log("Game Over!");
-            Destroy(other.gameObject);
-        } 
-
-        // if player collides with money, fireworks
-        else if (other.gameObject.CompareTag("Money"))
-        {
-            fireworksParticle.Play();
-            playerAudio.PlayOneShot(moneySound, 1.0f);
-            Destroy(other.gameObject);
+            Physics.gravity *= gravityModifier;
+            playerAudio = GetComponent<AudioSource>();
+            playerRb = GetComponent<Rigidbody>();
+            // Apply a small upward force at the start of the game
+            playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
 
         }
 
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            if (transform.position.y < yLowerRange)
+            {
+                transform.position = new Vector3(transform.position.x, yLowerRange, transform.position.z);
+                playerRb.AddForce(Vector3.up * floatForce * 5, ForceMode.Impulse);
+                playerAudio.PlayOneShot(bounceSound, 1.0f);
+            }
+            if (transform.position.y > yUpperRange)
+            {
+                transform.position = new Vector3(transform.position.x, yUpperRange, transform.position.z);
+            }
 
+            // While space is pressed and player is low enough, float up
+            if (Input.GetKey(KeyCode.Space) && !gameOver && transform.position.y <= yUpperRange - 1)
+            {
+                playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
+            }
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            // if player collides with bomb, explode and set gameOver to true
+            if (other.gameObject.CompareTag("Bomb"))
+            {
+                explosionParticle.Play();
+                playerAudio.PlayOneShot(explodeSound, 1.0f);
+                gameOver = true;
+                Debug.Log("Game Over!");
+                Destroy(other.gameObject);
+            }
+
+            // if player collides with money, fireworks
+            else if (other.gameObject.CompareTag("Money"))
+            {
+                fireworksParticle.Play();
+                playerAudio.PlayOneShot(moneySound, 1.0f);
+                Destroy(other.gameObject);
+
+            }
+
+        }
+
+    }
 }
--- Assets/Challenge 3/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 3/Scripts/SpawnManagerX.cs
@@ -1,34 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class SpawnManagerX : MonoBehaviour
+namespace ChallengeThree
 {
-    public GameObject[] objectPrefabs;
-    private float spawnDelay = 2;
-    private float spawnInterval = 1.5f;
-
-    private PlayerControllerX playerControllerScript;
-
-    // Start is called before the first frame update
-    void Start()
+    public class SpawnManagerX : MonoBehaviour
     {
-        InvokeRepeating("PrawnsObject", spawnDelay, spawnInterval);
-        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
-    }
+        public GameObject[] objectPrefabs;
+        private float spawnDelay = 2;
+        private float spawnInterval = 1.5f;
 
-    // Spawn obstacles
-    void SpawnObjects ()
-    {
-        // Set random spawn location and random object index
-        Vector3 spawnLocation = new Vector3(30, Random.Range(5, 15), 0);
-        int index = Random.Range(0, objectPrefabs.Length);
+        private PlayerControllerX playerControllerScript;
 
-        // If game is still active, spawn new object
-        if (!playerControllerScript.gameOver)
+        // Start is called before the first frame update
+        void Start()
         {
-            Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
+            InvokeRepeating("SpawnObjects", spawnDelay, spawnInterval);
+            playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
         }
 
+        // Spawn obstacles
+        void SpawnObjects()
+        {
+            // Set random spawn location and random object index
+            Vector3 spawnLocation = new Vector3(30, Random.Range(5, 15), 0);
+            int index = Random.Range(0, objectPrefabs.Length);
+
+            // If game is still active, spawn new object
+            if (!playerControllerScript.gameOver)
+            {
+                Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
+            }
+
+        }
     }
 }
