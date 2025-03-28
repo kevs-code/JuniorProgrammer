d04d5c4966361fc382b18b4603a42da9f8f1882b
kevs-code <kevscode@hotmail.com>
2025-03-28 10:09:41 +0000

    add animations

--- Assets/Scripts/Prototype 3/PlayerController.cs
+++ Assets/Scripts/Prototype 3/PlayerController.cs
@@ -5,23 +5,26 @@ namespace PrototypeThree
     public class PlayerController : MonoBehaviour
     {
         private Rigidbody playerRb;
-        public float jumpForce = 7f;
-        public float gravityModifier = 1f;
+        public float jumpForce = 700f; // 7
+        public float gravityModifier = 1.5f;
         public bool isOnGround = true;
         public bool gameOver = false;
+        private Animator playerAnim;
 
         void Start()
         {
             playerRb = GetComponent<Rigidbody>();
+            playerAnim = GetComponent<Animator>();
             Physics.gravity *= gravityModifier;
         }
 
         void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+            if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
             {
                 playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
                 isOnGround = false;
+                playerAnim.SetTrigger("Jump_trig");
             }
         }
 
@@ -35,6 +38,8 @@ namespace PrototypeThree
             {
                 gameOver = true;
                 Debug.Log("Game Over!");
+                playerAnim.SetBool("Death_b", true);
+                playerAnim.SetInteger("DeathType_int", 1);
             }
         }
     }
